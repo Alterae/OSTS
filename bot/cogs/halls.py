a3ceb696f777f071e78798c9218e189eeaeee96d
Glacier <ttoasterrr@gmail.com>
2021-07-22 12:57:40 -0600

    Made hall events happen

--- bot/cogs/halls.py
+++ bot/cogs/halls.py
@@ -20,7 +20,164 @@ class Halls(commands.Cog, description=""):
 
 
 	# ==================================================
-	# 
+	# Reaction event
+	# ==================================================
+	async def reaction_event(self, payload):
+		# ==================================================
+		# Get original channel and message from payload
+		# Get reaction counts
+		# ==================================================
+		original_channel = self.osts.get_channel(payload.channel_id)
+		original_message = await original_channel.fetch_message(payload.message_id)
+		reaction_count = None
+		for reaction in original_message.reactions:
+			if str(reaction.emoji) == str(payload.emoji):
+				reaction_count = reaction.count
+
+		if not reaction_count:
+			reaction_count = 0
+
+		# ==================================================
+		# Grab the servers data
+		# ==================================================
+		server_data = helpers.get_toml(f"servers/{original_message.guild.id}")
+
+		# ==================================================
+		# If the server doesnt have any halls, return
+		# ==================================================
+		if not server_data.get("halls") or server_data.get("halls") == {}:
+			return False
+
+		# ==================================================
+		# Iterate through halls and get the hall that matches 
+		# the reactions emoji
+		# ==================================================
+		emoji_is_hall_emoji = False
+		message_stored = False
+		hall = None
+		hall_data = None
+		for _hall, _hall_data in server_data["halls"].items():
+			if _hall_data["emoji"] == str(payload.emoji):
+				emoji_is_hall_emoji = True
+				hall = _hall
+				hall_data = _hall_data
+			for _message in _hall_data["messages"]:
+				if _message["original message id"] == original_message.id:
+					message_stored = True
+
+		# ==================================================
+		# If there isnt a hall that matches the reaction emoji
+		# Return
+		# ==================================================
+		if not emoji_is_hall_emoji:
+			return False
+
+		# ==================================================
+		# If the hall doesnt have a channel set, return
+		# ==================================================
+		if hall_data["channel"] == "":
+			return False
+
+		# ==================================================
+		# If the reaction count is greater than the requirement
+		# And the server data doesnt have the message stored
+		# ==================================================
+		if reaction_count >= server_data["halls"][hall]["requirement"] and not message_stored:
+			# ==================================================
+			# Announce that they got into a hall
+			# ==================================================
+			announcement_message = re.sub(r"\[author\]", original_message.author.name, server_data["halls"][hall]["announcement"])
+			announcement_message = re.sub(r"\[hall\]", hall, announcement_message)
+			announcement_message = re.sub(r"\[channel\]", original_message.channel.name, announcement_message)
+			await original_message.channel.send(announcement_message)
+
+			# ==================================================
+			# Send the message in the hall w/o proxy
+			# ==================================================
+			if not server_data["halls"][hall]["proxied"]:
+				hall_message = re.sub(r"\[author\]", original_message.author.name, server_data["halls"][hall]["format"])
+				hall_message = re.sub(r"\[message\]", original_message.content, hall_message)
+				hall_message = re.sub(r"\[attachments\]", "\n".join([attachment.url for attachment in original_message.attachments]), hall_message)
+				hall_channel = self.osts.get_channel(int(server_data["halls"][hall]["channel"][2:-1]))
+				hall_message_obj = await hall_channel.send(hall_message)
+			# else:
+				# Do proxy stuff
+
+			# ==================================================
+			# Add the message to the channels message list
+			# ==================================================
+			server_data["halls"][hall]["messages"].append({
+				"original message id": original_message.id,
+				"hall message id": hall_message_obj.id,
+				"author id": original_message.author.id
+			})
+
+			# ==================================================
+			# Log to console
+			# Save data
+			# ==================================================
+			helpers.log(
+				"Got put in a hall",
+				cog = self.cog_name,
+				ctx = original_message
+			)
+
+			helpers.set_toml(f"servers/{original_message.guild.id}", server_data)
+
+		# ==================================================
+		# If the reaction count is less than the requirement
+		# And the server data has the message stored
+		# ==================================================
+		if reaction_count < server_data["halls"][hall]["requirement"] and message_stored:
+			# ==================================================
+			# Announce that they got removed from the hall
+			# ==================================================
+			announcement_message = re.sub(r"\[author\]", original_message.author.name, server_data["halls"][hall]["removal announcement"])
+			announcement_message = re.sub(r"\[hall\]", hall, announcement_message)
+			announcement_message = re.sub(r"\[channel\]", original_message.channel.name, announcement_message)
+			await original_message.channel.send(announcement_message)
+
+			# ==================================================
+			# Remove the message from the halls data
+			# ==================================================
+			for i in range(len(server_data["halls"][hall]["messages"])):
+				if server_data["halls"][hall]["messages"][i]["original message id"] == original_message.id:
+					hall_message_id = server_data["halls"][hall]["messages"][i]["hall message id"]
+					author_message_id = server_data["halls"][hall]["messages"][i]["author id"]
+					server_data["halls"][hall]["messages"].pop(i)
+					break
+
+			# ==================================================
+			# Delete the actual message
+			# ==================================================
+			hall_channel = self.osts.get_channel(int(server_data["halls"][hall]["channel"][2:-1]))
+			hall_message = await hall_channel.fetch_message(hall_message_id)
+			await hall_message.delete()
+
+			# ==================================================
+			# Log to console
+			# Save data
+			# ==================================================
+			helpers.log(
+				"Got removed from a hall",
+				cog = self.cog_name,
+				ctx = original_message
+			)
+
+			helpers.set_toml(f"servers/{original_message.guild.id}", server_data)
+
+	
+	@commands.Cog.listener()
+	async def on_raw_reaction_add(self, payload):
+		await self.reaction_event(payload)
+
+	@commands.Cog.listener()
+	async def on_raw_reaction_remove(self, payload):
+		await self.reaction_event(payload)
+	
+	
+	# ==================================================
+	# Halls command
 	# ==================================================
 	@commands.command(brief="Add, edit, list, and remove halls!", help="""\
 		__**Halls**__
@@ -65,7 +222,7 @@ class Halls(commands.Cog, description=""):
 			if not server_data.get("halls") or server_data.get("halls") == {}:
 				return await helpers.give_output(
 					embed_title = helpers.error_title,
-					embed_content = f"This server doesn't have any halls!\nAdd some with `{self.osts.command_prefix}halls add`",
+					embed_content = f"This server doesn't have any halls!\nAdd some with `{self.osts.command_prefix(self.osts, ctx.message)}halls add`",
 					ctx = ctx
 				)
 
@@ -90,6 +247,7 @@ class Halls(commands.Cog, description=""):
 			return await helpers.give_output(
 				embed = embed,
 				log_text = f"Listed halls",
+				cog = self.cog_name,
 				ctx = ctx
 			)
 
@@ -104,7 +262,7 @@ class Halls(commands.Cog, description=""):
 			if _hall == "":
 				return await helpers.give_output(
 					embed_title = helpers.error_title,
-					embed_content = f"Please enter a hall name!\n`{self.osts.command_prefix}halls add Fame`",
+					embed_content = f"Please enter a hall name!\n`{self.osts.command_prefix(self.osts, ctx.message)}halls add Fame`",
 					ctx = ctx
 				)
 
@@ -115,13 +273,13 @@ class Halls(commands.Cog, description=""):
 			# Create the hall with default values
 			# ==================================================
 			server_data["halls"][_hall] = {
-				"emoji": None,
+				"emoji": "None",
 				"requirement": 4,
-				"channel": None,
+				"channel": "None",
 				"format": "**[author]**\n[message]\n\n[attachments]",
 				"proxied": False,
 				"announcement": f"**[author]** was added to the hall of {_hall}!",
-				"removal announcement": f"**[author]** was removed from the hall of {_hall}",
+				"removal announcement": f"**[author]** was removed from the hall of {_hall}!",
 				"messages": []
 			}
 
@@ -148,6 +306,7 @@ class Halls(commands.Cog, description=""):
 				embed = embed,
 				log_text = f"Created the hall of {_hall}",
 				ctx = ctx,
+				cog = self.cog_name,
 				data = server_data,
 				data_file = f"servers/{ctx.guild.id}"
 			)
@@ -163,7 +322,7 @@ class Halls(commands.Cog, description=""):
 			if not server_data.get("halls") or server_data.get("halls") == {}:
 				return await helpers.give_output(
 					embed_title = helpers.error_title,
-					embed_content = f"This server doesn't have any halls!\nAdd some with `{self.osts.command_prefix}halls add`",
+					embed_content = f"This server doesn't have any halls!\nAdd some with `{self.osts.command_prefix(self.osts, ctx.message)}halls add`",
 					ctx = ctx
 				)
 
@@ -199,6 +358,7 @@ class Halls(commands.Cog, description=""):
 				return await helpers.give_output(
 					embed = embed,
 					log_text = f"Got information on the hall of {_hall}",
+					cog = self.cog_name,
 					ctx = ctx
 				)
 
@@ -238,6 +398,7 @@ class Halls(commands.Cog, description=""):
 					embed_content = f"Successfully changed the hall of {_hall}'s {_item} to {_value}!",
 					log_text = f"Changed the hall of {_hall}'s {_item}",
 					ctx = ctx,
+					cog = self.cog_name,
 					data = server_data,
 					data_file = f"servers/{ctx.guild.id}"
 				)
@@ -258,13 +419,14 @@ class Halls(commands.Cog, description=""):
 						ctx = ctx
 					)
 
-				server_data["halls"][_hall][_item] = [_value]
+				server_data["halls"][_hall][_item] = str(_value)
 
 				return await helpers.give_output(
 					embed_title = helpers.success_title,
 					embed_content = f"Successfully changed the heall of {_hall}'s {_item} to {_value}!",
 					log_text = f"Changed the hall of {_hall}'s {_item}",
 					ctx = ctx,
+					cog = self.cog_name,
 					data = server_data,
 					data_file = f"servers/{ctx.guild.id}"
 				)
@@ -291,6 +453,7 @@ class Halls(commands.Cog, description=""):
 					embed_content = f"Successfully changed the hall of {_hall}'s {_item} to {_value}!",
 					log_text = f"Changed the hall of {_hall}'s {_item}",
 					ctx = ctx,
+					cog = self.cog_name,
 					data = server_data,
 					data_file = f"servers/{ctx.guild.id}"
 				)
@@ -317,6 +480,7 @@ class Halls(commands.Cog, description=""):
 					embed_content = f"Successfully changed the hall of {_hall}'s {_item} to {_value}!",
 					log_text = f"Changed the hall of {_hall}'s {_item}",
 					ctx = ctx,
+					cog = self.cog_name,
 					data = server_data,
 					data_file = f"servers/{ctx.guild.id}"
 				)	
@@ -333,6 +497,7 @@ class Halls(commands.Cog, description=""):
 					embed_content = f"Proxying for the hall of {_hall} has been changed to {server_data['halls'][_hall][_item]}!",
 					log_text = f"Changed the hall of {_hall}'s proxying",
 					ctx = ctx,
+					cog = self.cog_name,
 					data = server_data,
 					data_file = f"servers/{ctx.guild.id}"
 				)
@@ -344,7 +509,7 @@ class Halls(commands.Cog, description=""):
 			if not server_data.get("halls") or server_data.get("halls") == {}:
 				return await helpers.give_output(
 					embed_title = helpers.error_title,
-					embed_content = f"This server doesn't have any halls!\nAdd some with `{self.osts.command_prefix}halls add`",
+					embed_content = f"This server doesn't have any halls!\nAdd some with `{self.osts.command_prefix(self.osts, ctx.message)}halls add`",
 					ctx = ctx
 				)
 
@@ -362,6 +527,7 @@ class Halls(commands.Cog, description=""):
 				embed_content = f"Removed the hall of {_hall}",
 				log_text = f"Removed the hall of {_hall}",
 				ctx = ctx,
+				cog = self.cog_name,
 				data = server_data,
 				data_file = f"servers/{ctx.guild.id}"
 			)
